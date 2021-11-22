99dd26ae154019b61789d289f8143575c9f8c7fe
ivanfsilva <ivanfs27@gmail.com>
2021-11-22 19:28:56 -0300

    implementing LoginSteps

--- src/test/java/br/com/ivanfsilva/leilao/acceptance/steps/LoginSteps.java
+++ src/test/java/br/com/ivanfsilva/leilao/acceptance/steps/LoginSteps.java
@@ -23,26 +23,30 @@ public class LoginSteps {
 
     @Quando("realiza login")
     public void realiza_login() {
-        leiloesPage = this.loginPage.realizaLoginComo("fulano", "pass");
+        leiloesPage = this.loginPage.realizaLoginComo( "fulano", "pass" );
     }
 
     @Entao("é redirecionado para a pagina de leiloes")
     public void é_redirecionado_para_a_pagina_de_leiloes() {
-        Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+        Assert.assertTrue( this.leiloesPage.estaNaPaginaDeLeiloes() );
+        browser.clean();
     }
 
 
     @Dado("o usuario invalido")
     public void o_usuario_invalido() {
-
+        browser = new Browser();
+        browser.seed();
+        loginPage = browser.getLoginPage();
     }
 
     @Quando("tenta se logar")
     public void tenta_se_logar() {
-
+        this.loginPage.realizaLoginComo( "fulano", "xpto" );
     }
     @Entao("continua na página de login")
     public void continua_na_página_de_login() {
-
+        Assert.assertTrue( this.loginPage.estaNaPaginaDeLoginComErro() );
+        browser.clean();
     }
 }
