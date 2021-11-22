1b086832e6d4840f30e276962cb3a802d71dd858
ivanfsilva <ivanfs27@gmail.com>
2021-11-22 19:15:00 -0300

    login feature e steps

---
+++ src/test/java/br/com/ivanfsilva/leilao/acceptance/steps/LoginSteps.java
@@ -0,0 +1,48 @@
+package br.com.ivanfsilva.leilao.acceptance.steps;
+
+import br.com.ivanfsilva.leilao.e2e.pages.Browser;
+import br.com.ivanfsilva.leilao.e2e.pages.LeiloesPage;
+import br.com.ivanfsilva.leilao.e2e.pages.LoginPage;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
+
+public class LoginSteps {
+
+    private Browser browser;
+    private LoginPage loginPage;
+    private LeiloesPage leiloesPage;
+
+    @Dado("o usuario valido")
+    public void o_usuario_valido() {
+        browser = new Browser();
+        browser.seed();
+        loginPage = browser.getLoginPage();
+    }
+
+    @Quando("realiza login")
+    public void realiza_login() {
+        leiloesPage = this.loginPage.realizaLoginComo("fulano", "pass");
+    }
+
+    @Entao("é redirecionado para a pagina de leiloes")
+    public void é_redirecionado_para_a_pagina_de_leiloes() {
+        Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+    }
+
+
+    @Dado("o usuario invalido")
+    public void o_usuario_invalido() {
+
+    }
+
+    @Quando("tenta se logar")
+    public void tenta_se_logar() {
+
+    }
+    @Entao("continua na página de login")
+    public void continua_na_página_de_login() {
+
+    }
+}
